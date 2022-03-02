969e51a0d1a158abc509b04058b2dc664676d486
Samuel <samuel.cflau@gmail.com>
2022-03-02 17:17:19 +0800

    57. Search Users 5 分鐘 (GithubContext.js; searchUsers; fetchUsers)

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -22,19 +22,23 @@ export const GithubProvider = ({ children }) => {
   // Set loading
   const setLoading = () => dispatch({ type: 'SET_LOADING' })
 
-  // Get initial users (testing purposes)
-  const fetchUsers = async () => {
+  // Get search results
+  const searchUsers = async (text) => {
     setLoading()
 
-    const response = await fetch(`${GITHUB_URL}/users`, {
+    const params = new URLSearchParams({
+      q: text,
+    })
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
       },
     })
 
-    const data = await response.json()
+    const { items } = await response.json() // from Github API
 
-    console.log(data)
+    console.log(items)
     // When using useState
     // setUsers(data)
     // setIsLoading(false)
@@ -42,13 +46,37 @@ export const GithubProvider = ({ children }) => {
     // When switch to useReducer
     dispatch({
       type: 'GET_USERS',
-      payload: data,
+      payload: items,
     })
   }
 
+  // Get initial users (testing purposes)
+  // const fetchUsers = async () => {
+  //   setLoading()
+
+  //   const response = await fetch(`${GITHUB_URL}/users`, {
+  //     headers: {
+  //       Authorization: `token ${GITHUB_TOKEN}`,
+  //     },
+  //   })
+
+  //   const data = await response.json()
+
+  //   console.log(data)
+  //   // When using useState
+  //   // setUsers(data)
+  //   // setIsLoading(false)
+
+  //   // When switch to useReducer
+  //   dispatch({
+  //     type: 'GET_USERS',
+  //     payload: data,
+  //   })
+  // }
+
   return (
     <GithubContext.Provider
-      value={{ users: state.users, isLoading: state.isLoading, fetchUsers }}
+      value={{ users: state.users, isLoading: state.isLoading, searchUsers }}
     >
       {children}
     </GithubContext.Provider>
