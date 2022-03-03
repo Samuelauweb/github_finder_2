4be11465599d50429bf089f1a9d69d8d1cc1cda0
Samuel <samuel.cflau@gmail.com>
2022-03-03 17:24:24 +0800

    65. Repo Items 8 分鐘 (RepoItem.jsx)

--- src/components/repos/RepoList.jsx
+++ src/components/repos/RepoList.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import RepoItem from './RepoItem'
 
 function RepoList({ repos }) {
+  // 'repos' from User.jsx
+
   return (
     <div className='rounded-lg shadow-lg card bg-base-100'>
       <div className='card-body'>
@@ -9,7 +11,7 @@ function RepoList({ repos }) {
           Latest Repositories
         </h2>
         {repos.map((repo) => {
-          return <h3>{repo.name}</h3>
+          return <RepoItem key={repo.id} repo={repo} />
         })}
       </div>
     </div>
